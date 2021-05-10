9e630511e948c228538ea26c000a6b504a379a7a
Nickb <nick.barty@outlook.com>
2021-05-10 19:03:24 +1000

    - Re-style to be more mobile friendly - Reduce words where possible for less information overload

--- views/hecs_calculator.py
+++ views/hecs_calculator.py
@@ -68,25 +68,26 @@ def _hecs_calculator():
                     f"${user_hecs_tax.user_income_hecs_tax_amount:,} ({user_hecs_tax.user_tax_bracket_tax_rate}%)"))
             display_strings.append(Markup(f"${weekly_repayments:,.0f}"))
             display_strings.append(Markup(f"{round(index_rate * 100, 2)}%"))
-            display_strings.append(Markup("<br>"))
             display_strings.append(Markup(
-                f"Approximate voluntary repayment length: <b>{int(len(voluntary_values) / 12)} years {len(voluntary_values) % 12} months</b> "
+                f"Approximate voluntary repayment length:<br> <b>{int(len(voluntary_values) / 12)} years {len(voluntary_values) % 12} months</b> "
                 f"<i>(${hecs_debt + total_voluntary_index :,.2f} total debt)</i>"))
             display_strings.append(Markup(
-                f"Approximate involuntary repayment length: <b>{int(len(involuntary_values) / 12)} years {len(involuntary_values) % 12} months</b> "
+                f"Approximate involuntary repayment length:<br> <b>{int(len(involuntary_values) / 12)} years {len(involuntary_values) % 12} months</b> "
                 f"<i>(${hecs_debt + total_involuntary_index:,.2f} total debt)</i>"))
-            display_strings.append(Markup("<br>"))
             year_difference = int(len(involuntary_values) / 12) - int(len(voluntary_values) / 12)
+            try:
+                times_quicker = len(involuntary_values) / len(voluntary_values)
+            except ZeroDivisionError:
+                times_quicker = 0
             display_strings.append(
                 Markup(
-                    f"It is approximately <b>{len(involuntary_values) / len(voluntary_values):.2f}x</b> <i>({year_difference} years)</i> faster "
+                    f"It is approximately <b>{times_quicker:.2f}x</b> <i>({year_difference} years)</i> faster "
                     f"and <b>${total_involuntary_index - total_voluntary_index:,.2f} cheaper</b> "
-                    f"to make <b>${weekly_repayments} weekly payments</b> "
+                    f"to make <b>${weekly_repayments:,.2f} weekly payments</b> "
                     f"<i>(${weekly_repayments * 52:,.2f} Annually)</i>"))
             display_strings.append(Markup(
                 f"You would earn approximately <b>${user_hecs_tax.user_income_hecs_tax_amount * year_difference:,.2f}</b> "
                 f"over the course of the <b>{year_difference} years</b> you would have been paying mandatory HECS tax"))
-            display_strings.append(Markup("<br>"))
             display_strings.append(
                 Markup(f"With the combined savings and extra earnings, you would have approximately "
                        f"<span style='color:green'><b>${((user_hecs_tax.user_income_hecs_tax_amount * year_difference) * 0.675) + (total_involuntary_index - total_voluntary_index):,.2f}</b> more in your pocket "
@@ -96,18 +97,18 @@ def _hecs_calculator():
         except RecursionError:
             if annual_income < user_hecs_tax.tax_brackets_min:
                 error_strings.append(Markup(
-                    f"Your annual income of <b>${annual_income:,}</b> is not high enough to automatically pay mandatory HECS debt tax<br>"
-                    f"The minimum annual income for automatic repayments is <b>${user_hecs_tax.tax_brackets_min:,.2f}</b><br>"
-                    f"You should voluntarily repay <b>${(hecs_debt * index_rate) / 52:,.2f} per week (${hecs_debt * index_rate:,.2f} Annually)</b> "
-                    f"at a minimum to negate average annual loan indexation<br>"
-                    f"A good general rule is to set aside enough money to offset indexation and pay a little extra off the loan. <i>Usually 2-5% of you annual income will cover this</i><br>"
+                    f"Your annual income of <b>${annual_income:,}</b> is not high enough to automatically pay HECS debt tax<br><br>"
+                    f"The minimum annual income for automatic repayments is <b>${user_hecs_tax.tax_brackets_min:,.2f}</b><br><br>"
+                    f"At a minimum, you should voluntarily repay <b>${(hecs_debt * index_rate) / 52:,.2f} per week (${hecs_debt * index_rate:,.2f} Annually)</b> "
+                    f"to negate average annual loan indexation<br><br>"
+                    f"A good rule of thumb is to set aside enough money to offset indexation and pay a little extra off the loan. <i>Usually</i> 2-5% of you annual income will cover this<br><br>"
                     f"In this case, <b>2-5%</b> of <u>${annual_income:,.2f}</u> is <b>${annual_income * 0.02:,.2f}-${annual_income * 0.05:,.2f}</b>"))
             else:
                 error_strings.append(Markup(
-                    f"Your annual income of <b>${annual_income:,.2f}</b> is likely not high enough to repay your HECS debt of <b>${hecs_debt:,.2f}</b> <span style='color:red'>without voluntary repayments</span><br>"
+                    f"Your annual income of <b>${annual_income:,.2f}</b> is likely not high enough to repay your HECS debt of <b>${hecs_debt:,.2f}</b> <span style='color:red'>without voluntary repayments</span><br><br>"
                     f"You should voluntarily repay <b>${(hecs_debt * index_rate) / 52:,.2f} per week (${hecs_debt * index_rate:,.2f} Annually)</b> "
-                    f"at a minimum to negate average annual loan indexation<br>"
-                    f"Your specified weekly voluntary repayments of <b>${weekly_repayments:,.2f}</b> total <b>${weekly_repayments * 52:,.2f}</b> annually<br>"
+                    f"at a minimum to negate average annual loan indexation<br><br>"
+                    f"Your specified weekly voluntary repayments of <b>${weekly_repayments:,.2f}</b> total <b>${weekly_repayments * 52:,.2f}</b> annually<br><br>"
                     f"Choosing to <b>not make any voluntary repayments</b> will <b>exponentially increase the amount of time</b> it takes you to pay off your debt!"))
 
     return render_template(f"{blueprint_name}.html", form=form, display_strings=display_strings,
